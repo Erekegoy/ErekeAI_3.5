d6f35e1c981b94422fb6fe747332ec565c7cacb0
Erlan <bakytzanoverlan234@gmail.com>
2026-07-04 22:49:14 +0500

    Temporarily reduce Milestone2Module

--- app/src/main/java/com/erekeai/core/di/Milestone2Module.kt
+++ app/src/main/java/com/erekeai/core/di/Milestone2Module.kt
@@ -1,30 +1,15 @@
 package com.erekeai.core.di
 
-import com.erekeai.backup.BackupManager
-import com.erekeai.backup.FileBackupManager
-import com.erekeai.build.BuildRunner
-import com.erekeai.build.NoOpBuildRunner
 import com.erekeai.notifier.ChangeNotifier
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import java.io.File
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object Milestone2Module {
-
-    @Provides
-    @Singleton
-    fun provideBackupManager(): BackupManager =
-        FileBackupManager(File(".erekeai-backups"))
-
-    @Provides
-    @Singleton
-    fun provideBuildRunner(): BuildRunner =
-        NoOpBuildRunner()
 
     @Provides
     @Singleton
