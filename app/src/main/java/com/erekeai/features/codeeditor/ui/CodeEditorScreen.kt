498f8a1f8f75d02eb4c0812441d448cf4bb7c9b9
Erlan <bakytzanoverlan234@gmail.com>
2026-07-04 05:32:55 +0500

    ...

--- app/src/main/java/com/erekeai/features/codeeditor/ui/CodeEditorScreen.kt
+++ app/src/main/java/com/erekeai/features/codeeditor/ui/CodeEditorScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 
+import androidx.compose.ui.viewinterop.AndroidView
+import io.github.rosemoe.sora.widget.CodeEditor
 
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.erekeai.features.codeeditor.viewmodel.CodeEditorViewModel
