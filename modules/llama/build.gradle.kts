5d36ebe4354b2846a72551abc5422bb806ffa83a
Erlan <bakytzanoverlan234@gmail.com>
2026-07-11 22:47:28 +0500

    Lower llama minSdk to 26

--- modules/llama/build.gradle.kts
+++ modules/llama/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     
     defaultConfig {
-        minSdk = 33
+        minSdk = 26
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
